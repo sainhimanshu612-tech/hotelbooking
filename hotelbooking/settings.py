39eec7d8fb25e920d3fba562082b1fd752b7ef9a
himanshusen <sainhimanshu612@gmail.com>
2026-07-16 15:15:07 +0530

    Update settings.py
    done

--- hotelbooking/settings.py
+++ hotelbooking/settings.py
@@ -136,5 +136,4 @@ STATICFILES_DIRS = [BASE_DIR /"static"]
 STATIC_ROOT=BASE_DIR/"staticfiles"
 
 MEDIA_URL = '/media/'
-import os 
-MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+MEDIA_ROOT = BASE_DIR/"media"
